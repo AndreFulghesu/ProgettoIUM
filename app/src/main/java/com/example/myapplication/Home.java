cf6358abb2531792a2200f92dd5228bc612d3d50
Faber <fabersau@gmail.com>
2020-03-03 16:44:04 +0100

    Aggiunta Form Lettura Capitolo, sistemazione bug

--- app/src/main/java/com/example/myapplication/Home.java
+++ app/src/main/java/com/example/myapplication/Home.java
@@ -2,13 +2,29 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-    }
+
+        Button continuaLettura = findViewById(R.id.continuaLettura);
+        Button catalogo = findViewById(R.id.catalogo);
+        Button myBook = findViewById(R.id.myLibro);
+
+        continuaLettura.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent continuaLettura = new Intent(Home.this, LeggiLibro.class);
+                startActivity(continuaLettura);
+            }
+        });
+        }
 }
