f4292cc9ea3ed2ed928200b8168b2afbfdb4751f
Andrea Fulghesu <fulghesu_a@yahoo.it>
2020-05-03 22:42:18 +0200

    Creazione lateral Menu nella HomePage Definizione Nuovi Colori Definizione nuovo Layout per HomePage molto più accattivante e sexy E tante altre cose... Forse la volta buona

--- app/src/main/java/com/example/myapplication/Home.java
+++ app/src/main/java/com/example/myapplication/Home.java
@@ -21,7 +21,7 @@ public class Home extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.drawer_home);
 
 
         drawer = findViewById(R.id.drawerHome);
